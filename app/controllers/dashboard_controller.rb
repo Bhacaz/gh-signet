50718f20c1c7a3b5d30f777afcd9d2b7397c85ea
Jean-Francis Bastien <jfbastien@petalmd.com>
2023-10-19 19:44:13 -0400

    same visual

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,10 +1,10 @@
 class DashboardController < ApplicationController
   def index
-    @pull_requests= {
-      'My Pull Requests' => auth_user.octokit.search_issues(*PrQueryLibrary::MY_PR_QUERY).items,
-      'Assigned to me' => auth_user.octokit.search_issues(*PrQueryLibrary::ASSIGN_TO_ME_QUERY).items,
-      'Review requests' => auth_user.octokit.search_issues(*PrQueryLibrary::REVIEW_REQUESTED_QUERY).items,
-      'Team review requests' => []
-    }
+    @pull_requests= [
+      { 'My Pull Requests' => auth_user.octokit.search_issues(*PrQueryLibrary::MY_PR_QUERY).items },
+      { 'Assigned to me' => auth_user.octokit.search_issues(*PrQueryLibrary::ASSIGN_TO_ME_QUERY).items },
+      { 'Review requests' => auth_user.octokit.search_issues(*PrQueryLibrary::REVIEW_REQUESTED_QUERY).items },
+      { 'Team review requests' => [] }
+    ]
   end
 end
