4b4db65257854b2057294aa33d546d4be09f8172
Jean-Francis Bastien <jfbastien@petalmd.com>
2023-10-18 20:43:09 -0400

    it look like the old one

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class DashboardController < ApplicationController
   def index
+    @@cache ||= auth_user.octokit.search_issues(*PrQueryLibrary::MY_PR_QUERY).items
+    @pull_requests = @@cache
   end
 end
