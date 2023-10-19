66eda8ec4a0a842099c81ef7a6e4ae8dad35de39
Jean-Francis Bastien <jfbastien@petalmd.com>
2023-10-18 21:27:51 -0400

    Other query

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,10 @@
 class DashboardController < ApplicationController
   def index
-    @@cache ||= auth_user.octokit.search_issues(*PrQueryLibrary::MY_PR_QUERY).items
-    @pull_requests = @@cache
+    @pull_requests= {
+      'My Pull Requests' => auth_user.octokit.search_issues(*PrQueryLibrary::MY_PR_QUERY).items,
+      'Assigned to me' => auth_user.octokit.search_issues(*PrQueryLibrary::ASSIGN_TO_ME_QUERY).items,
+      'Review requests' => auth_user.octokit.search_issues(*PrQueryLibrary::REVIEW_REQUESTED_QUERY).items,
+      'Team review requests' => []
+    }
   end
 end
--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -4,4 +4,12 @@ module DashboardHelper
     elements = pull_request_url.split('/').last(4)
     elements[0] + '/' + elements[1] + '#' + elements[3]
   end
+
+  def text_color_contrast(hex_color)
+    r = hex_color[1..2].to_i(16)
+    g = hex_color[3..4].to_i(16)
+    b = hex_color[5..6].to_i(16)
+    yiq = ((r*299)+(g*587)+(b*114))/1000
+    yiq >= 128 ? 'black' : 'white'
+  end
 end
