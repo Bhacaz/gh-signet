997f7727626eee8fb17fa5629ac93bf72333e4b4
Jean-Francis Bastien <jfbastien@petalmd.com>
2023-10-20 21:02:10 -0400

    Delete stream

--- app/controllers/pr_signets_controller.rb
+++ app/controllers/pr_signets_controller.rb
@@ -1,4 +1,5 @@
 class PrSignetsController < ApplicationController
+  before_action :set_record, only: %i[edit update destroy]
   def index
     @pr_signets = auth_user.pr_signets
   end
@@ -7,39 +8,41 @@ class PrSignetsController < ApplicationController
     @pr_signet = PrSignet.new
   end
 
-  def show
-  end
-
   def create
     @pr_signet = PrSignet.new(user: auth_user, **pr_signet_params)
     if @pr_signet.save
-      redirect_to dashboard_path
+      redirect_to action: :index
     else
       render :new
     end
   end
 
   def edit
-    @pr_signet = PrSignet.find(params[:id])
   end
 
   def update
-    @pr_signet = PrSignet.find(params[:id])
     if @pr_signet.update(pr_signet_params)
-      redirect_to dashboard_path
+      redirect_to action: :index
     else
       render :edit
     end
   end
 
   def destroy
-    PrSignet.find(params[:id]).destroy
-    redirect_to action: :index
+    @pr_signet.destroy
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.remove(@pr_signet) }
+      format.html { redirect_to action: :index }
+    end
   end
 
   private
 
   def pr_signet_params
     params.require(:pr_signet).permit(:query, :title, :sort, :order)
+  end
+
+  def set_record
+    @pr_signet = PrSignet.find(params[:id])
   end
 end
