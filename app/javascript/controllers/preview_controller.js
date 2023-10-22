9631d00a2a2bbd3594cd83f6f6b72b2f5f0d15a2
Jean-Francis Bastien <jfbastien@petalmd.com>
2023-10-21 23:00:10 -0400

    modals works

--- app/javascript/controllers/preview_controller.js
+++
@@ -1,7 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-export default class extends Controller {
-  hide() {
-    document.getElementById('modal-preview').style.display = 'none';
-    document.getElementsByClassName('modal-backdrop')[0].style.display = 'none';
-  }
-}
