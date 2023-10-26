3b88a36c4ccc0483d1249d74bf47bfbd1d85ba12
Jean-Francis Bastien <jfbastien@petalmd.com>
2023-10-25 21:46:21 -0400

    Status

--- app/controllers/pull_requests_controller.rb
+++ app/controllers/pull_requests_controller.rb
@@ -1,6 +1,39 @@
 class PullRequestsController < ApplicationController
   def details
     @pull_request = auth_user.octokit.pull_request(params[:repo], params[:number])
-    # turbo_stream.replace(:pull_request_details_panel)
+    sha = @pull_request.head.sha
+    @statuses = []
+    auth_user.octokit.check_runs_for_ref(params[:repo], sha).check_runs.each do |check|
+      @statuses << check_to_status(check)
+    end
+
+    auth_user.octokit.combined_status(params[:repo], sha).statuses.each do |status|
+      @statuses << status_to_status(status)
+    end
   end
+
+  private
+  Status = Struct.new(:name, :avatar_url, :description, :context, :target_url, :conclusion)
+
+  def status_to_status(status)
+    Status.new(
+      status.context,
+      status.avatar_url,
+      status.description,
+      status.context,
+      status.target_url,
+      status.state
+    )
+  end
+  def check_to_status(check)
+    Status.new(
+      check.name,
+      check.app.owner.avatar_url,
+      '',
+      check.context,
+      check.details_url,
+      check.conclusion
+    )
+  end
+
 end
